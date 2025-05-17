69939cc375975a1d6da31f94e687ac1c720d5f45
Amaar Fazlani <fazlaniamaar@gmail.com>
2025-05-17 13:10:27 -0500

    Add stores endpoint

---
+++ app/controllers/stores_controller.rb
@@ -0,0 +1,56 @@
+class StoresController < ApplicationController
+  before_action :find_store, only: [:show, :update, :destroy]
+
+  def index
+    @stores = Store.all
+
+    puts "*******************"
+    puts @stores.count
+    puts "*******************"
+
+    render json: { data: @stores, status: 200 }
+  end
+
+  def create
+    @store = Store.new(store_params)
+
+    if @store.save
+      render json: { data: @store }, status: 200
+    else
+      render json: { errors: @store.errors }, status: 422
+    end
+  end
+
+  def update
+    @store = Store.new(store_params)
+
+    if @store.update(store_parameters)
+    render json: { data: @store, status: 200 }
+    else
+      render json: { errors: @store.errors }, status: 422
+    end
+  end
+
+  def destroy
+    if @store.destroy
+      render json: { data: @store, status: 200 }
+    else
+      render json: { errors: @store.errors }, status: 422
+    end
+  end
+
+  def show
+    render json: { data: @store, status: 200 }
+  end
+
+  private
+
+  def store_params
+    params.require(:store).permit(:name, :address, :description)
+  end
+
+
+  def find_store
+    @store = Store.find(params[:id])
+  end
+end
