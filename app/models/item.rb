8da356ae46f5d5f93ea620a4d31cfee509b1f5ff
Amaar Fazlani <fazlaniamaar@gmail.com>
2025-05-19 11:11:30 -0500

    Add validation to models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
   has_many :ingredients
+
+  validates :name, presence: true
+  validates :price, presence: true
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,6 @@
 class Store < ApplicationRecord
   has_many :items
+
+  validates :name, presence: true
+  validates :address, presence: true
 end
