ed3131f32d0f1a6ee87745b9d4f0e9b34b10a7d1
Amaar Fazlani <fazlaniamaar@gmail.com>
2025-05-19 15:17:31 -0500

    Clean up views

--- client/src/components/StoreForm.vue
+++ client/src/components/StoreForm.vue
@@ -33,7 +33,6 @@
           v-model="description"
           id="description"
           class="w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          required
         ></textarea>
       </div>
 
--- client/src/components/Stores.vue
+++ client/src/components/Stores.vue
@@ -1,6 +1,4 @@
 <template>
-  <Toast :show="showToast" />
-
   <div class="max-w-7xl mx-auto px-4 py-6">
     <div class="flex items-center justify-between mb-6">
       <h1 class="text-2xl font-bold">Stores</h1>
